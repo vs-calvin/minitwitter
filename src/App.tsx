a2a0bbfd62761567913cf78bea25f00f55e9b0cc
vs-calvin <calvin@versusnow.io>
2022-02-09 15:38:07 +0900

    finished tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
 import React, { useState } from "react";
 import Feed from "./components/Feed";
-import { createPost } from "./model";
+import { createPost, PostEntry } from "./model";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import NewPost from "./components/NewPost";
 
+export function dumpPosts(entries: PostEntry[]) {
+  return JSON.stringify(entries);
+}
+
+export function loadPosts(json: string): PostEntry[] {
+  const parsed: any[] = JSON.parse(json);
+  for (const item of parsed) {
+    item.createdAt = new Date(item.createdAt);
+  }
+  return parsed;
+}
+
 function App() {
-  const [entries, setEntries] = useState([
-    createPost({ content: "Hello world", createdAt: new Date() }),
-    createPost({ content: "Another post", createdAt: new Date() }),
-  ]);
+  const [entries, setEntries] = useState(() => {
+    let loaded = localStorage["entries"];
+    if (loaded) {
+      return loadPosts(loaded);
+    }
+    return [
+      createPost({ content: "Hello world", createdAt: new Date() }),
+      createPost({ content: "Another post", createdAt: new Date() }),
+    ];
+  });
+
   return (
     <BrowserRouter>
       <Routes>
         <Route
           path={"/post"}
           element={
-            <NewPost onAdd={(entry) => setEntries([entry, ...entries])} />
+            <NewPost
+              onAdd={(entry) => {
+                setEntries([entry, ...entries]);
+                localStorage["entries"] = dumpPosts([entry, ...entries]);
+              }}
+            />
           }
         />
         <Route path={"/"} element={<Feed entries={entries} />}></Route>
